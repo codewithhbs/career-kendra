086c4388577d1a6825a218ba5e88e38a992ec73f
Coderhitesh <hiteshy468@gmail.com>
2026-04-29 12:19:56 +0530

    add

--- server/src/models/message.js
+++ server/src/models/message.js
@@ -1,5 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
-  const Message = sequelize.define("Message", {
+  const Message = sequelize.define("message", {
     id: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
